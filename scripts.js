7ee29504e20cdc63f619aa67b6d233d42341fb86
KHansen22 <kjalen@CLEA112-01.uvic.ca>
2015-08-02 18:00:01 -0700

    added/fixed comments

--- scripts.js
+++ scripts.js
@@ -14,17 +14,20 @@
         supports.placeholder = 'placeholder' in input;
 
         if(!supports.autofocus){
+            //no autofocus
             console.log("no autofocus support")
         }
         if(!supports.required){
+            //no required
             console.log("no required support")
         }
         if(!supports.placeholder){
+            //no placeholder
             console.log("no placeholder support")
         }
 
     
-        //create send object for detecting button press, *** This doesn't work right now   ***
+        //create send object for detecting button press, the button will change to ...Sending regardless of if it actually is or not.
         var send = document.getElementById("register-submit");
         if(send){
             send.onclick = function () {
